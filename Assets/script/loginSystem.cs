f39aeb66297a65c9589c055835b1ea95e84cf7da
liuxq <lxqiang87@126.com>
2015-11-07 22:27:50 +0800

    12

--- Assets/script/loginSystem.cs
+++ Assets/script/loginSystem.cs
@@ -11,10 +11,15 @@ public class loginSystem : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         DontDestroyOnLoad(gameObject.transform);
+        KBEngine.Event.registerOut("onConnectStatus", this, "onConnectStatus");
 	}
-    void onLogin()
+    public void onConnectStatus(bool beSuccess)
     {
-
+        
+    }
+    public void onLogin()
+    {
+        KBEngine.Event.fireIn("login", if_userName.text, if_passWord.text, System.Text.Encoding.UTF8.GetBytes("2015.11.7"));
     }
 	// Update is called once per frame
 	void Update () {
