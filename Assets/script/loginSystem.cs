38b004be05b65f52cdf6b59c9e9ea93b90aee5d4
liuxq <lxqiang87@gmail.com>
2015-11-07 11:39:40 +0800

    ogigin

---
+++ Assets/script/loginSystem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using KBEngine;
+using UnityEngine.UI;
+
+public class loginSystem : MonoBehaviour {
+    public InputField if_userName;
+    public InputField if_passWord;
+    public Button bt_login;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(gameObject.transform);
+	}
+    void onLogin()
+    {
+
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
