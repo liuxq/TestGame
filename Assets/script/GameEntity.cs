73518a7c15b30d78a967d0b64f3ef12dafce43ba
liuxq <lxqiang87@126.com>
2015-12-04 18:00:34 +0800

    12

---
+++ Assets/script/GameEntity.cs
@@ -0,0 +1,302 @@
+﻿
+using UnityEngine;
+using KBEngine;
+using System.Collections;
+using System;
+using System.Xml;
+using System.Collections.Generic;
+
+public class GameEntity : MonoBehaviour
+{
+    public bool isPlayer = false;
+
+    private Vector3 _position = Vector3.zero;
+    private Vector3 _eulerAngles = Vector3.zero;
+    private Vector3 _scale = Vector3.zero;
+
+    public Vector3 destPosition = Vector3.zero;
+    public Vector3 destDirection = Vector3.zero;
+
+    private float _speed = 0f;
+    private byte jumpState = 0;
+    private float currY = 1.0f;
+
+    private Camera playerCamera = null;
+
+    public string entity_name;
+
+    public string hp = "100/100";
+
+    float npcHeight = 2.0f;
+
+    public CharacterController characterController;
+
+    public bool isOnGround = true;
+
+    public bool entityEnabled = true;
+
+    void Awake()
+    {
+    }
+
+    void Start()
+    {
+        characterController = ((UnityEngine.GameObject)gameObject).GetComponent<CharacterController>();
+    }
+
+    void OnGUI()
+    {
+        if (!gameObject.transform.FindChild("Graphics").GetComponent<MeshRenderer>().GetComponent<Renderer>().isVisible)
+            return;
+
+        Vector3 worldPosition = new Vector3(transform.position.x, transform.position.y + npcHeight, transform.position.z);
+
+        if (playerCamera == null)
+            playerCamera = Camera.main;
+
+        //根据NPC头顶的3D坐标换算成它在2D屏幕中的坐标
+        Vector2 uiposition = playerCamera.WorldToScreenPoint(worldPosition);
+
+        //得到真实NPC头顶的2D坐标
+        uiposition = new Vector2(uiposition.x, Screen.height - uiposition.y);
+
+        //计算NPC名称的宽高
+        Vector2 nameSize = GUI.skin.label.CalcSize(new GUIContent(entity_name));
+
+        //设置显示颜色为黄色
+        GUI.color = Color.yellow;
+
+        //绘制NPC名称
+        GUI.Label(new Rect(uiposition.x - (nameSize.x / 2), uiposition.y - nameSize.y - 5.0f, nameSize.x, nameSize.y), entity_name);
+
+        //计算NPC名称的宽高
+        Vector2 hpSize = GUI.skin.label.CalcSize(new GUIContent(hp));
+
+        //设置显示颜色为红
+        GUI.color = Color.red;
+
+        //绘制HP
+        GUI.Label(new Rect(uiposition.x - (hpSize.x / 2), uiposition.y - hpSize.y - 30.0f, hpSize.x, hpSize.y), hp);
+    }
+
+    public Vector3 position
+    {
+        get
+        {
+            return _position;
+        }
+
+        set
+        {
+            _position = value;
+
+            if (gameObject != null)
+                gameObject.transform.position = _position;
+        }
+    }
+
+    public Vector3 eulerAngles
+    {
+        get
+        {
+            return _eulerAngles;
+        }
+
+        set
+        {
+            _eulerAngles = value;
+
+            if (gameObject != null)
+            {
+                gameObject.transform.eulerAngles = _eulerAngles;
+            }
+        }
+    }
+
+    public Quaternion rotation
+    {
+        get
+        {
+            return Quaternion.Euler(_eulerAngles);
+        }
+
+        set
+        {
+            eulerAngles = value.eulerAngles;
+        }
+    }
+
+    public Vector3 scale
+    {
+        get
+        {
+            return _scale;
+        }
+
+        set
+        {
+            _scale = value;
+
+            if (gameObject != null)
+                gameObject.transform.localScale = _scale;
+        }
+    }
+
+    public float speed
+    {
+        get
+        {
+            return _speed;
+        }
+
+        set
+        {
+            _speed = value;
+        }
+    }
+
+    public void entityEnable()
+    {
+        entityEnabled = true;
+    }
+
+    public void entityDisable()
+    {
+        entityEnabled = false;
+    }
+
+    public void set_state(sbyte v)
+    {
+        if (v == 3)
+        {
+            if (isPlayer)
+                gameObject.transform.FindChild("Graphics").GetComponent<MeshRenderer>().material.color = Color.green;
+            else
+                gameObject.transform.FindChild("Graphics").GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+        else if (v == 0)
+        {
+            if (isPlayer)
+                gameObject.transform.FindChild("Graphics").GetComponent<MeshRenderer>().material.color = Color.blue;
+            else
+                gameObject.transform.FindChild("Graphics").GetComponent<MeshRenderer>().material.color = Color.white;
+        }
+        else if (v == 1)
+        {
+            gameObject.transform.FindChild("Graphics").GetComponent<MeshRenderer>().material.color = Color.black;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!entityEnabled)
+            return;
+
+        if (isPlayer == false && KBEngineApp.app != null)
+            return;
+
+        KBEngine.Event.fireIn("updatePlayer", gameObject.transform.position.x,
+            gameObject.transform.position.y, gameObject.transform.position.z, gameObject.transform.rotation.eulerAngles.y);
+    }
+
+    void Update()
+    {
+        if (!entityEnabled)
+        {
+            position = destPosition;
+            return;
+        }
+
+        float deltaSpeed = (speed * Time.deltaTime);
+
+        if (isPlayer == true)
+        {
+            characterController.stepOffset = deltaSpeed;
+
+            if (isOnGround != characterController.isGrounded)
+            {
+                KBEngine.Entity player = KBEngineApp.app.player();
+                player.isOnGround = characterController.isGrounded;
+                isOnGround = characterController.isGrounded;
+            }
+
+            return;
+        }
+
+        if (Vector3.Distance(eulerAngles, destDirection) > 0.0004f)
+        {
+            rotation = Quaternion.Slerp(rotation, Quaternion.Euler(destDirection), 8f * Time.deltaTime);
+        }
+
+        float dist = 0.0f;
+
+        if (isOnGround)
+        {
+            dist = Vector3.Distance(new Vector3(destPosition.x, 0f, destPosition.z),
+                new Vector3(position.x, 0f, position.z));
+        }
+        else
+        {
+            dist = Vector3.Distance(destPosition, position);
+        }
+
+        if (jumpState > 0)
+        {
+            if (jumpState == 1)
+            {
+                currY += 0.05f;
+
+                if (currY > 2.0f)
+                    jumpState = 2;
+            }
+            else
+            {
+                currY -= 0.05f;
+                if (currY < 1.0f)
+                {
+                    jumpState = 0;
+                    currY = 1.0f;
+                }
+            }
+
+            Vector3 pos = position;
+            pos.y = currY;
+            position = pos;
+        }
+
+        if (dist > 0.01f)
+        {
+            Vector3 pos = position;
+
+            Vector3 movement = destPosition - pos;
+            movement.y = 0f;
+            movement.Normalize();
+
+            movement *= deltaSpeed;
+
+            if (dist > deltaSpeed || movement.magnitude > deltaSpeed)
+                pos += movement;
+            else
+                pos = destPosition;
+
+            if (isOnGround)
+                pos.y = currY;
+
+            position = pos;
+        }
+        else
+        {
+        }
+    }
+
+    public void OnJump()
+    {
+        Debug.Log("jumpState: " + jumpState);
+
+        if (jumpState != 0)
+            return;
+
+        jumpState = 1;
+    }
+}
+
---
+++ Assets/script/World.cs
@@ -0,0 +1,153 @@
+﻿using KBEngine;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System; 
+
+public class World : MonoBehaviour {
+
+    public UnityEngine.GameObject entityPerfab;
+    public UnityEngine.GameObject avatarPerfab;
+
+    private UnityEngine.GameObject player = null;
+	// Use this for initialization
+	void Start () {
+        KBEngine.Event.registerOut("onAvatarEnterWorld", this, "onAvatarEnterWorld");
+        KBEngine.Event.registerOut("onEnterWorld", this, "onEnterWorld");
+        KBEngine.Event.registerOut("onLeaveWorld", this, "onLeaveWorld");
+        KBEngine.Event.registerOut("set_position", this, "set_position");
+        KBEngine.Event.registerOut("set_direction", this, "set_direction");
+        KBEngine.Event.registerOut("updatePosition", this, "updatePosition");
+	}
+    void OnDestroy()
+    {
+        KBEngine.Event.deregisterOut(this);
+    }
+	// Update is called once per frame
+	void Update () {
+        createPlayer();
+	}
+
+    public void onAvatarEnterWorld(UInt64 rndUUID, Int32 eid, KBEngine.Avatar avatar)
+    {
+        if (!avatar.isPlayer())
+        {
+            return;
+        }
+
+        //UI.inst.info("loading scene...(加载场景中...)");
+        Debug.Log("loading scene...");
+
+        //object speed = avatar.getDefinedPropterty("moveSpeed");
+        //if (speed != null)
+        //    set_moveSpeed(avatar, speed);
+
+        //object state = avatar.getDefinedPropterty("state");
+        //if (state != null)
+        //    set_state(avatar, state);
+
+        //object modelScale = avatar.getDefinedPropterty("modelScale");
+        //if (modelScale != null)
+        //    set_modelScale(avatar, modelScale);
+
+        //object name = avatar.getDefinedPropterty("name");
+        //if (name != null)
+        //    set_entityName(avatar, (string)name);
+
+        //object hp = avatar.getDefinedPropterty("HP");
+        //if (hp != null)
+        //    set_HP(avatar, hp);
+    }
+
+    public void onEnterWorld(KBEngine.Entity entity)
+    {
+        if (entity.isPlayer())
+            return;
+
+        float y = entity.position.y;
+        if (entity.isOnGround)
+            y = 1.3f;
+
+        entity.renderObj = Instantiate(entityPerfab, new Vector3(entity.position.x, y, entity.position.z),
+            Quaternion.Euler(new Vector3(entity.direction.y, entity.direction.z, entity.direction.x))) as UnityEngine.GameObject;
+
+        ((UnityEngine.GameObject)entity.renderObj).name = entity.className + "_" + entity.id;
+
+        set_position(entity);
+        set_direction(entity);
+
+        //object speed = entity.getDefinedPropterty("moveSpeed");
+        //if (speed != null)
+        //    set_moveSpeed(entity, speed);
+
+        //object state = entity.getDefinedPropterty("state");
+        //if (state != null)
+        //    set_state(entity, state);
+
+        //object modelScale = entity.getDefinedPropterty("modelScale");
+        //if (modelScale != null)
+        //    set_modelScale(entity, modelScale);
+
+        //object name = entity.getDefinedPropterty("name");
+        //if (name != null)
+        //    set_entityName(entity, (string)name);
+
+        //object hp = entity.getDefinedPropterty("HP");
+        //if (hp != null)
+        //    set_HP(entity, hp);
+    }
+
+    public void onLeaveWorld(KBEngine.Entity entity)
+    {
+        if (entity.renderObj == null)
+            return;
+
+        UnityEngine.GameObject.Destroy((UnityEngine.GameObject)entity.renderObj);
+        entity.renderObj = null;
+    }
+
+    public void set_position(KBEngine.Entity entity)
+    {
+        if (entity.renderObj == null)
+            return;
+
+        ((UnityEngine.GameObject)entity.renderObj).GetComponent<GameEntity>().destPosition = entity.position;
+        ((UnityEngine.GameObject)entity.renderObj).GetComponent<GameEntity>().position = entity.position;
+    }
+    public void set_direction(KBEngine.Entity entity)
+    {
+        if (entity.renderObj == null)
+            return;
+
+        ((UnityEngine.GameObject)entity.renderObj).GetComponent<GameEntity>().destDirection =
+            new Vector3(entity.direction.y, entity.direction.z, entity.direction.x);
+    }
+    public void createPlayer()
+    {
+        if (player != null)
+            return;
+
+        if (KBEngineApp.app.entity_type != "Avatar")
+        {
+            return;
+        }
+
+        KBEngine.Avatar avatar = (KBEngine.Avatar)KBEngineApp.app.player();
+        if (avatar == null)
+        {
+            Debug.Log("wait create(palyer)!");
+            return;
+        }
+
+        float y = avatar.position.y;
+        if (avatar.isOnGround)
+            y = 1.3f;
+
+        player = Instantiate(avatarPerfab, new Vector3(avatar.position.x, y, avatar.position.z),
+                             Quaternion.Euler(new Vector3(avatar.direction.y, avatar.direction.z, avatar.direction.x))) as UnityEngine.GameObject;
+
+        player.GetComponent<GameEntity>().entityDisable();
+        avatar.renderObj = player;
+        ((UnityEngine.GameObject)avatar.renderObj).GetComponent<GameEntity>().isPlayer = true;
+    }
+}
