6b31baa9c273958ed8e59c860d9dfa92486ebb56
奋斗小Q <lxqiang87@gmail.com>
2018-07-10 08:46:41 +0800

    grass

--- Assets/script/MoveController.cs
+++ Assets/script/MoveController.cs
@@ -11,9 +11,12 @@ public class MoveController : MonoBehaviour {
     private float minLen = 0f;
     private int skillId = 1;
     private UnityEngine.GameObject UIGame;
+
+    private CharacterController cc;
     void Start()
     {
         animator = GetComponent<Animator>();
+        cc = GetComponent<CharacterController>();
         sf = Camera.main.GetComponent<SmoothFollow>();
         hasDes = false;
 
@@ -45,7 +48,6 @@ public class MoveController : MonoBehaviour {
         {
             animator.speed = 1.0f;
             animator.SetFloat("Speed", 0.0f);
-            //GetComponent<Animation>().CrossFade("idle");
         }
         else if (move.joystickName == "RotJoystick")
         {
@@ -67,9 +69,12 @@ public class MoveController : MonoBehaviour {
 
                 Vector3 dir = r * new Vector3(joyPositionX, 0, joyPositionY);
                 transform.LookAt(new Vector3(transform.position.x + dir.x, transform.position.y, transform.position.z + dir.z));
-                
+
                 //移动玩家的位置（按朝向位置移动）
-                transform.Translate(Vector3.forward * Time.deltaTime * 5);
+                //transform.Translate(Vector3.forward * Time.deltaTime * 5);
+                cc.Move(transform.forward * Time.deltaTime * 5);
+
+                Shader.SetGlobalVector("_Player1Pos", transform.position + new Vector3(500,1,500));
 
                 //移动摄像机
                 sf.FollowUpdate();
@@ -78,23 +83,6 @@ public class MoveController : MonoBehaviour {
                 animator.SetFloat("Speed", 1.0f);
             }
         }
-        //else if (move.joystickName == "RotJoystick")
-        //{
-        //    if (joyPositionY != 0 || joyPositionX != 0)
-        //    {
-        //        sf.rotate += joyPositionX*2;
-        //        if (sf.rotate < -180.0f)
-        //            sf.rotate += 360.0f;
-        //        if (sf.rotate > 180.0f)
-        //            sf.rotate -= 360.0f;
-        //        sf.height -= joyPositionY;
-        //        if (sf.height < .5f)
-        //            sf.height = .5f;
-        //        if (sf.height > 15.0f)
-        //            sf.height = 15.0f;
-               
-        //    }
-        //}
     }
     void Update()
     {
@@ -110,7 +98,9 @@ public class MoveController : MonoBehaviour {
             else
             {
                 transform.LookAt(moveDes);
-                transform.Translate(Vector3.forward * Time.deltaTime * 5);
+                //transform.Translate(Vector3.forward * Time.deltaTime * 5);
+                cc.Move(transform.forward * Time.deltaTime * 5);
+                Shader.SetGlobalVector("_Player1Pos", transform.position + new Vector3(500, 1, 500));
                 //移动摄像机
                 sf.FollowUpdate();
                 //播放奔跑动画
